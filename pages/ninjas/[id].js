53b2ca7ad11f8c419759b3631eee4bd691b6b0e2
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2021-02-03 15:33:15 -0500

    Create dynamic paths with getStaticPaths and fetch individual data

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -2,24 +2,41 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import ninjaStyles from '../../styles/Ninja.module.scss';
 
-// export const getStaticProps = async () => {
-//   const router = useRouter();
-//   const { id } = router.query;
+export const getStaticProps = async context => {
+  const id = context.params.id;
+  const res = await fetch(`http://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
 
-//   const res = await fetch(`http://jsonplaceholder.typicode.com/users/${id}`);
-//   const data = await res.json();
+  return {
+    props: {
+      ninja: data
+    }
+  };
+};
 
-//   return {
-//     props: {
-//       ninja: data
-//     }
-//   };
-// };
+export const getStaticPaths = async () => {
+  const res = await fetch('http://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
 
-const Ninja = () => {
+  const paths = data.map(ninja => {
+    return {
+      params: { id: ninja.id.toString() }
+    };
+  });
+
+  return {
+    paths,
+    fallback: false
+  };
+};
+
+const Ninja = ({ ninja }) => {
   return (
     <div className={ninjaStyles.ninja}>
-      <h1>Ninja Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>Username: {ninja.username}</p>
+      <p>Website: {ninja.website}</p>
+      <p>Company: {ninja.company.name}</p>
     </div>
   );
 };
